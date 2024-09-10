735981871fcc7e7fd5af438c1d2e90b393dbec5e
Alexandre-Lucas-Dias <alexandrelucasdiascosta@yahoo.com.br>
2024-09-09 23:09:18 -0300

    atualizando

--- Objetos/objetos.js
+++ Objetos/objetos.js
@@ -1,1 +1,12 @@
-//asd
+const listaCPFs = ['123213213', '1231531523521'] // funciona bem para guardar um só tipo de informação
+
+const infosPessoa = ['nome', 'José Silva', 'idade', 32]
+
+console.log(infosPessoa[1]);
+
+const objPessoa = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '12345678901',
+    turma: 'JavaScript'
+} // objetos funcionam melhor para descrever a "realidade"
